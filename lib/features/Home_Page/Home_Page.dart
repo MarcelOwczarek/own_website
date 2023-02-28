12465c3b22c7ec60482751509d087d628bab32d1
Marcel <owczarek2000@gmail.com>
2023-02-28 23:35:48 +0100

    Add content to Container in Home_Page

--- lib/features/Home_Page/Home_Page.dart
+++ lib/features/Home_Page/Home_Page.dart
@@ -51,7 +51,6 @@ class Home_Page extends StatelessWidget {
                 text[2],
                 const Custom_SizedBox(),
                 const about_me(),
-                //const Custom_SizedBox(),
                 const SizedBox(
                   height: 150,
                 ),
@@ -69,15 +68,52 @@ class Home_Page extends StatelessWidget {
                   ),
                   padding: const EdgeInsets.all(20),
                   margin: const EdgeInsets.all(20),
-                  height: 400,
-                  width: 340,
-                  child: const Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text(''),
+                  height: 560,
+                  width: 350,
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.asset(
+                              'images/picture2.jpg',
+                              width: 180,
+                              height: 180,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Align(
+                              alignment: Alignment.topLeft,
+                              child: Text('Hello',
+                                  style: GoogleFonts.poppins(
+                                      color: Colors.white))),
+                          const SizedBox(height: 5),
+                          Padding(
+                            padding: const EdgeInsets.all(1),
+                            child: Text(
+                              "I'm Marcel, I'm 22 years old and I'm glad you visited me. I am not afraid of new challenges and technologies, I appreciate change and development. I bravely take risks to achieve my goals. I can adapt to any situation to deal with problems. I am friendly and sociable, confident in myself and my beliefs. My main interests are new technologies, programming, gym, traveling and cinema. I started my adventure with the Flutter framework in June 2022.",
+                              style: GoogleFonts.poppins(color: Colors.white),
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          Align(
+                              alignment: Alignment.bottomRight,
+                              child: Text('Best Regards Marcel',
+                                  style: GoogleFonts.poppins(
+                                      color: Colors.white))),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
                 const SizedBox(
-                  height: 100,
+                  height: 50,
                 ),
                 Text(
                   'Made with 💙 by Marcel Owczarek using Flutter',
