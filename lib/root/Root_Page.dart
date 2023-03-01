b5fa2cadcad940b29f6efe2c2b6e8196cb2ca80c
Marcel <owczarek2000@gmail.com>
2023-03-01 23:10:45 +0100

    Add Projects_Page to project

--- lib/root/Root_Page.dart
+++ lib/root/Root_Page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: camel_case_types, file_names, non_constant_identifier_names, avoid_web_libraries_in_flutter, library_private_types_in_public_api
 
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:own_website/features/Home_Page/Home_Page.dart';
 import 'package:own_website/features/texts/texts.dart';
 import 'package:own_website/features/widgets/Custom_NavigationDrawer.dart';
@@ -42,9 +43,37 @@ class _MainPageState extends State<MainPage> {
       case NavigationItem.homepage:
         return const Home_Page();
       case NavigationItem.projects:
-        return const Scaffold();
+        return const Projects_Page();
       case NavigationItem.contact:
         return const Scaffold();
     }
+  }
+}
+
+class Projects_Page extends StatelessWidget {
+  const Projects_Page({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 25),
+            Text(
+              'My Projects:',
+              style: GoogleFonts.raleway(
+                fontSize: 20,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 25),
+          ],
+        )
+      ],
+    );
   }
 }
