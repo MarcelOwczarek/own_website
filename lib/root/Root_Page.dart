59292db0927db10f2434b7054c6ea079d5c7d0ad
Marcel <owczarek2000@gmail.com>
2023-03-02 10:51:54 +0100

    Add Contact_button for Contact_Page and divider

--- lib/root/Root_Page.dart
+++ lib/root/Root_Page.dart
@@ -59,9 +59,73 @@ class _MainPageState extends State<MainPage> {
       case NavigationItem.projects:
         return const Projects_Page();
       case NavigationItem.contact:
-        return const Scaffold();
+        return const Contact_Page();
     }
   }
+}
+
+class Contact_Page extends StatelessWidget {
+  const Contact_Page({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Contact_button(context,
+            icon: Icons.mail_outline, text: 'flutter@marcelowczarek.dev'),
+        const divider(),
+        Contact_button(context, icon: Icons.code, text: 'My profile on Github'),
+        const divider(),
+        Contact_button(context,
+            icon: Icons.smartphone_outlined, text: '+48 794 116 095'),
+      ],
+    );
+  }
+}
+
+class divider extends StatelessWidget {
+  const divider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20),
+      child: Divider(thickness: 2, color: Colors.indigoAccent),
+    );
+  }
+}
+
+Widget Contact_button(
+  BuildContext context, {
+  required IconData icon,
+  required String text,
+}) {
+  return SizedBox(
+    height: 60,
+    width: 250,
+    child: ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.indigo,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(32))),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, size: 20),
+          Text(
+            text,
+            style: GoogleFonts.lato(color: Colors.white, fontSize: 14),
+          ),
+        ],
+      ),
+    ),
+  );
 }
 
 class Projects_Page extends StatelessWidget {
